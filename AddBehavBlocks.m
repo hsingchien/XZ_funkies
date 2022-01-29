7c561155c8934114c1e9af8d1337575e5f120405
rustynerve <hsingchien.zhang@gmail.com>
2022-01-29 15:39:41 -0800

    no message

--- AddBehavBlocks.m
+++ AddBehavBlocks.m
@@ -9,25 +9,36 @@ xrange = tax.XLim;
 yrange = tax.YLim;
 ylow = yrange(1);
 yhigh = yrange(2);
+% ylow = 15;
+% yhigh = 18;
 if strcmp('all',blist)
     blist = B.EventNames;
 end
+blist(find(strcmp(blist,'other'))) = [];
+
 for b = 1:length(blist)
     bname = blist{b};
    % find the index of behavior
    bindx = find(strcmp(B.EventNames, bname));
    % onset time and offset time
-   bonset = B.OnsetTimes{bindx};
-   boffset = B.OffsetTimes{bindx};
-   bonsettime = Btimestamp(bonset);
-   boffsettime = Btimestamp(boffset);
+   bonset = B.OnsetTimes{bindx}; % bstructure index start from 0
+   boffset = B.OffsetTimes{bindx}; % bstructure index start from 0
+   bonsettime = Btimestamp(bonset+1);
+   boffsettime = Btimestamp(boffset+1);
    for k = 1:length(bonsettime)
       this_onset = bonsettime(k);
       this_offset = boffsettime(k);
-      P = patch(tax,[this_onset this_onset this_offset this_offset],[ylow, yhigh, yhigh, ylow],clist(b,:),'EdgeColor',clist(b,:), 'FaceAlpha', 0.5);       
+      P = patch(tax,[this_onset this_onset this_offset this_offset],[ylow, yhigh, yhigh, ylow],clist(b,:),'EdgeColor',[1,1,1], 'FaceAlpha', 0.3);       
    end
     
     
+end
+
+block = round(range(xrange)/length(clist));
+for i = 1:length(blist)
+    ll = (i-1) * block; 
+    patch([ll, ll, ll+block, ll+block], [yhigh+1, yhigh+3, yhigh+3, yhigh+1], clist(i,:),'EdgeColor',clist(i,:), 'FaceAlpha', 0.3);
+    text(ll, yhigh+2, blist{i},'FontSize', 10);
 end
 
 
