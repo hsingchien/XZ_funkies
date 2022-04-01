02bc0192c37672bef769f89690fd4c5df7833073
rustynerve <hsingchien.zhang@gmail.com>
2022-04-01 10:16:40 -0700

    no message

--- renameVid.m
+++ renameVid.m
@@ -6,7 +6,7 @@ for i = 1:length(avis)
     fname = temp{1};
     digit_i = regexp(fname, '\d*');
     digi_n = str2num(fname(digit_i(end):end));
-    movefile([num2str(digi_n),'.avi'], ['XZ121\', num2str(digi_n+18),'.avi']);
+    movefile([num2str(digi_n),'.avi'], ['XZ152\', num2str(digi_n+28),'.avi']);
     avi_id_list = [avi_id_list, digi_n];
-end
+end 
   
